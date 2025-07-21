a321e542ce595658e7146ee4df2c7b0da240e108
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-21 16:06:11 +0200

    solution

--- src/Triangle.hs
+++ src/Triangle.hs
@@ -1,4 +1,10 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows x = map (\r -> map (`pascal` r) [0 .. r]) [0 .. fromIntegral (x - 1)]
+
+pascal :: Integer -> Integer -> Integer
+pascal c r =
+  if (r == 0) || (r == c) || (c == 0)
+    then 1
+    else pascal (c - 1) (r - 1) + pascal c (r - 1)
